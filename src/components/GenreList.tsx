6dcb2364b05cc1fd16898ec3dddd63b956f617a1
SKSritharan <sk.sritharan75@gmail.com>
2024-09-04 23:08:38 +0530

    Created a generic data fetching hook

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,14 +1,13 @@
-import React from "react";
 import useGenres from "../hooks/useGenres";
 
 const GenreList = () => {
-  const { genre, error, isLoading } = useGenres();
+  const { data, error, isLoading } = useGenres();
   return (
     <div>
       {isLoading && <p>Loading...</p>}
       {error && <p>{error}</p>}
       <ul>
-        {genre.map((genre) => (
+        {data.map((genre) => (
           <li key={genre.id}>{genre.name}</li>
         ))}
       </ul>
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,12 +1,4 @@
-import { useState, useEffect } from "react";
-import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
-
-export interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
+import useData from "./useData";
 
 export interface Genre {
   id: number;
@@ -14,40 +6,6 @@ export interface Genre {
   slug: string;
 }
 
-interface GenreResponse {
-  count: number;
-  results: Genre[];
-}
-
-const useGenres = () => {
-  const [genre, setGenre] = useState<Genre[]>([]);
-  const [isLoading, setLoading] = useState(false);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    const controller = new AbortController();
-
-    setLoading(true);
-    apiClient
-      .get<GenreResponse>("/genres", {
-        signal: controller.signal,
-      })
-      .then((res) => {
-        setGenre(res.data.results);
-        setLoading(false);
-      })
-      .catch((err) => {
-        {
-          if (err instanceof CanceledError) return;
-          setError(err.message);
-          setLoading(false);
-        }
-      });
-
-    return () => controller.abort();
-  }, []);
-
-  return { genre, error, isLoading };
-};
+const useGenres = () => useData<Genre>("/genres");
 
 export default useGenres;
