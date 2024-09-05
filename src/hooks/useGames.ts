3bc04e305eef583ae8bb4eaff366eb665fb8614c
SKSritharan <sk.sritharan75@gmail.com>
2024-09-05 13:31:22 +0530

    Refactor : To use GameQuery object

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,3 +1,4 @@
+import { GameQuery } from "../App";
 import useData from "./useData";
 import { Genre } from "./useGenres";
 import { Platform } from "./usePlatforms";
@@ -10,14 +11,16 @@ export interface Game {
   metacritic: number;
 }
 
-const useGames = (
-  selectedGenre: Genre | null,
-  selectedPlatform: Platform | null
-) =>
+const useGames = (gamequery: GameQuery) =>
   useData<Game>(
     "/games",
-    { params: { genres: selectedGenre?.id, platforms: selectedPlatform?.id } },
-    [selectedGenre?.id, selectedPlatform?.id]
+    {
+      params: {
+        genres: gamequery.genre?.id,
+        platforms: gamequery.platform?.id,
+      },
+    },
+    [gamequery.genre?.id, gamequery.platform?.id]
   );
 
 export default useGames;
